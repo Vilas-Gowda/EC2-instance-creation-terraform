166da02ed9ec79d1e4487d499824f6691307db83
Vilas Gowda <vilasgowda65@gmail.com>
2023-08-23 20:09:54 +0530

    asd

--- assignment2.tf
+++ assignment2.tf
@@ -1,19 +1,21 @@
 provider "aws" {
     region = "us-east-1"
-    access_key = "############"
-    secret_key = "############"
+    access_key = "##############"
+    secret_key = "##############"
 }
 
 resource "aws_vpc" "main" {
 
     cidr_block = "10.0.0.0/16"
-
+    tags = {
+            name = "demo"
+        }
 }
 
 
 resource "aws_subnet" "subnet1" {
 
-    vpc_id = aws.vpc.main.id
+    vpc_id = aws_vpc.main.id
     cidr_block = "10.0.0.0/24"
 
 
@@ -21,13 +23,13 @@ resource "aws_subnet" "subnet1" {
 
 resource "aws_subnet" "subnet2" {
 
-    vpc_id = aws.vpc.main.id
+    vpc_id = aws_vpc.main.id
     cidr_block = "10.0.1.0/24"
     map_public_ip_on_launch = true
 
 }
 
-resource "aws_security_group" "demo_group"{
+resource "aws_security_group" "demo_group" {
 
     name = "demo_group"
     description = "defining SHH ports for inbound and outbound traffic"    
@@ -60,15 +62,14 @@ resource "aws_security_group" "demo_group"{
 
 
 resource "aws_instance" "demo1" {
-    ami = "ami-085925f297f89fce1"
-    instace_type = "t2.micro"
-    subnet_id     = aws_subnet.public_subnet.id
+    ami = "ami-053b0d53c279acc90"
+    instance_type = "t2.micro"
 
     security_groups = [aws_security_group.demo_group.name]
 
     root_block_device {
         volume_type = "gp2"
-        volume_size = 5
+        volume_size = 8
         encrypted = false 
     }
 
